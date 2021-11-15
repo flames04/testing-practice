d94941cd2b75aab11163179fbf0fb0aa52be553f
Ea Francisco <emberkasai04@gmail.com>
2021-11-15 15:05:40 -0500

    finish tst 5 of product

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -33,12 +33,13 @@ try {
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
   var result = product(2);
-  if (result !== 2) throw new Error('Expected product(2) to be 4. Received: ' + result);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
